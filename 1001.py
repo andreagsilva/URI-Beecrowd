3f0b9ad3dc606d83e3c6472b6e28c57d82ff8a64
andreagsilva <andreasilva.and@gmail.com>
2022-03-25 01:00:33 -0300

    Resolução URI programação 1

--- 1001.py
+++ 1001.py
@@ -1,4 +1,4 @@
 A = int(input())
 B = int(input())
 X=A+B
-print ('X = {}'.format(X))
+print (' X = {}'.format(X))
--- 1002.py
+++ 1002.py
@@ -1,4 +1,4 @@
 pi=3.14159
 raio=float(input())
 area=(raio**2)*pi
-print ("A=%.4f"%area)
+print ("A =%.4f"%area)
